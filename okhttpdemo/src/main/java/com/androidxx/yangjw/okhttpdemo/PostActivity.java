d8ee70197629fb158cf4ade61751b49deb611cbf
yangjw <yangwen8701@126.com>
2016-06-28 15:37:55 +0800

    OkHttp基本使用

--- okhttpdemo/src/main/java/com/androidxx/yangjw/okhttpdemo/PostActivity.java
+++ okhttpdemo/src/main/java/com/androidxx/yangjw/okhttpdemo/PostActivity.java
@@ -2,6 +2,7 @@ package com.androidxx.yangjw.okhttpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,8 +12,10 @@ import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -28,9 +31,11 @@ import okhttp3.Response;
 public class PostActivity extends AppCompatActivity {
 
     public static final String URL = "http://www.1688wan.com/majax.action?method=searchGift";
+    public static final String URL_LOCAL = "http://10.6.151.93:8080/WebServer/login.do";
     private Button mAsyncBtn;
     private TextView mShowResultTxt;
     private OkHttpClient okHttpClient = new OkHttpClient();
+    private Button mJsonBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +48,18 @@ public class PostActivity extends AppCompatActivity {
     private void initView() {
         mAsyncBtn = (Button) findViewById(R.id.okhttp_post_async_btn);
         mShowResultTxt = (TextView) findViewById(R.id.okhttp_post_show_result_tv);
+        mJsonBtn = (Button) findViewById(R.id.okhttp_post_json_btn);
+        mJsonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendJsonData();
+            }
+        });
         mAsyncBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                asyncRequest();
+//                asyncRequest();
+                asyncMyRequest();
             }
         });
     }
@@ -79,6 +92,64 @@ public class PostActivity extends AppCompatActivity {
                         mShowResultTxt.setText(result);
                     }
                 });
+            }
+        });
+    }
+
+    private void asyncMyRequest() {
+        //封装Post请求的参数
+        FormBody formBody = new FormBody.Builder()
+                .add("username","lisi22222")//添加Post请求的参数
+                .add("password","123456")
+                .build();
+        //创建一个Request对象
+        Request request = new Request.Builder()
+                .url(URL_LOCAL) //网络请求地址
+                .post(formBody) //Post请求，并且将Post请求需要的参数封装到FormBody中
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mShowResultTxt.setText(result);
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 发送一个Json格式的参数
+     */
+    private void sendJsonData() {
+        //告诉服务器传递的是一个Json格式的数据
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        //创建RequestBody
+        String json = "{'price':12.3}";
+        RequestBody requestBody = RequestBody.create(mediaType, json);
+
+        Request request = new Request.Builder()
+                .url(URL_LOCAL)
+                .post(requestBody) // 传递一个Json格式的数据
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("androidxx", "onResponse: " + response.body().string());
             }
         });
     }
