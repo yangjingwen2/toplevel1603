58d7fe5d5f642a315bea66133d25825f62c5b41a
yangjw <yangwen8701@126.com>
2016-06-29 18:09:59 +0800

    ButterKnife的使用

---
+++ butterknifedemo/src/main/java/com/androidxx/yangjw/butterknifedemo/constant/UrlConfig.java
@@ -0,0 +1,9 @@
+package com.androidxx.yangjw.butterknifedemo.constant;
+
+/**
+ * Created by yangjw on 2016/6/29.
+ */
+public class UrlConfig {
+
+    public static final String BANNER_URL = "http://api.liwushuo.com/v2/banners";
+}
--- butterknifedemo/src/main/java/com/androidxx/yangjw/butterknifedemo/fragment/WellChosenFragment.java
+++ butterknifedemo/src/main/java/com/androidxx/yangjw/butterknifedemo/fragment/WellChosenFragment.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +16,16 @@ import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidxx.yangjw.butterknifedemo.BannerInfo;
 import com.androidxx.yangjw.butterknifedemo.R;
+import com.androidxx.yangjw.butterknifedemo.constant.UrlConfig;
+import com.androidxx.yangjw.httplibrary.IOKCallBack;
+import com.androidxx.yangjw.httplibrary.OkHttpTool;
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,9 +50,12 @@ public class WellChosenFragment extends Fragment {
     ExpandableListView expandableListView;
 
     private Map<String,List<String>> datas = new HashMap<>();
+    private List<BannerInfo.DataBean.BannersBean> imageDatas = new ArrayList<>();
     private List<String> groupNameDatas = new ArrayList<>();
     private Context mContext;
     private MyExpadnListAdapter myExpadnListAdapter;
+    public HeaderRVAdapter headerRVAdapter;
+    HeaderViewHolder headerViewHolder;
 
     /**
      * Use this factory method to create a new instance of
@@ -69,14 +83,139 @@ public class WellChosenFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_well_chosen, container, false);
         ButterKnife.bind(this,view);
+        setupHeaderView();
         setupExpandListView();
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //开始自动滚动
+        headerViewHolder.convenientBanner.startTurning(2000);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //停止滚动
+        headerViewHolder.convenientBanner.stopTurning();
+    }
+
+    /**
+     * 添加头部布局
+     */
+    private void setupHeaderView() {
+        View headerView = LayoutInflater.from(mContext).inflate(R.layout.wellchosen_header_view, null);
+        headerViewHolder = new HeaderViewHolder(headerView);
+        setupHeaderRecyclerView(headerViewHolder);
+        loadBannerDatas();//动态加载数据
+        setupBanner(headerViewHolder);
+        expandableListView.addHeaderView(headerView);
+
+    }
+
+    private void loadBannerDatas() {
+        OkHttpTool.newInstance().start(UrlConfig.BANNER_URL).callback(new IOKCallBack() {
+            @Override
+            public void success(String result) {
+                Gson gson = new Gson();
+                BannerInfo bannerInfo = gson.fromJson(result, BannerInfo.class);
+                imageDatas.addAll(bannerInfo.getData().getBanners());
+                headerViewHolder.convenientBanner.getViewPager().getAdapter().notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void setupBanner(HeaderViewHolder headerViewHolder) {
+
+        headerViewHolder.convenientBanner.setPages(new CBViewHolderCreator<HeaderBannerViewHolder>() {
+            @Override
+            public HeaderBannerViewHolder createHolder() {
+                return new HeaderBannerViewHolder();
+            }
+        },imageDatas)
+        .setPageIndicator(new int[]{R.drawable.btn_check_disabled_nightmode,R.drawable.btn_check_normal})
+        .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT);
+    }
+
+    class HeaderBannerViewHolder implements Holder<BannerInfo.DataBean.BannersBean> {
+        ImageView imageView;
+        @Override
+        public View createView(Context context) {
+             imageView = new ImageView(context);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, int position, BannerInfo.DataBean.BannersBean data) {
+//            imageView.setImageResource(data.getImage_url());
+            Picasso.with(mContext).load(data.getImage_url()).into(imageView);
+        }
+    }
+
+    /**
+     * 初始化头部RecyclerView
+     * @param headerViewHolder
+     */
+    private void setupHeaderRecyclerView(HeaderViewHolder headerViewHolder) {
+//        headerViewHolder.mRecyclerView
+        //创建布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext,LinearLayoutManager.HORIZONTAL,false);
+        headerViewHolder.mRecyclerView.setLayoutManager(linearLayoutManager);
+        //创建一个适配器
+        headerRVAdapter = new HeaderRVAdapter();
+        headerViewHolder.mRecyclerView.setAdapter(headerRVAdapter);
+    }
+
+    class HeaderViewHolder {
+
+        @BindView(R.id.header_view_rv)
+        RecyclerView mRecyclerView;
+        @BindView(R.id.header_view_cb)
+        ConvenientBanner convenientBanner;
+
+        public HeaderViewHolder(View headerView) {
+            ButterKnife.bind(this,headerView);
+        }
+    }
+
+
+
+    //创建ViewHolder
+    class HeaderRVViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public HeaderRVViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView;
+        }
+    }
+
+    //创建适配器
+    class HeaderRVAdapter extends RecyclerView.Adapter<HeaderRVViewHolder> {
+
+        @Override
+        public HeaderRVViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            ImageView imageView = new ImageView(mContext);
+            return new HeaderRVViewHolder(imageView);
+        }
+
+        @Override
+        public void onBindViewHolder(HeaderRVViewHolder holder, int position) {
+            holder.imageView.setImageResource(R.mipmap.ic_launcher);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
     }
 
     /**
      * 初始化ExpandableListView
      */
     private void setupExpandListView() {
+
         //1、创建数据源
         setupDatas();
         //2、创建适配器
@@ -99,6 +238,9 @@ public class WellChosenFragment extends Fragment {
     }
 
     private void setupDatas() {
+        if (groupNameDatas != null && !groupNameDatas.isEmpty()) {
+            return;
+        }
         for (int i = 0; i < 20; i++) {
             String groupName = "GROUP" + i;
             groupNameDatas.add(groupName);
